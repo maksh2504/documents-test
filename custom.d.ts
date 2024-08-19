2b955bbf2d26949c0f8548223c3c6ae56b76c82d
maksim.sheitser@foundevs.com <maksim.sheitser@foundevs.com>
2024-08-19 19:27:26 +0200

    install dependencies

---
+++ custom.d.ts
@@ -0,0 +1,25 @@
+declare module '*.svg' {
+  import * as React from 'react';
+
+  export const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & { title?: string }
+  >;
+
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpg' {
+  const path: string;
+  export default path;
+}
+
+declare module '*.jpeg' {
+  const path: string;
+  export default path;
+}
+
+declare module '*.png' {
+  const path: string;
+  export default path;
+}
