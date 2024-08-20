bbd90ed45b154c533c5fe9b5d6a2901ef5098fba
maksim.sheitser@foundevs.com <maksim.sheitser@foundevs.com>
2024-08-20 16:23:30 +0200

    refactoring

--- src/features/Home/components/CareerGoal/CareerGoal.test.tsx
+++ src/features/Home/components/CareerGoal/CareerGoal.test.tsx
@@ -1,18 +1,10 @@
 import { screen, waitFor } from '@testing-library/react';
-import { renderWithProvider, mockServer, careerGoal } from 'tests';
+import { renderWithProvider, mockServer } from 'tests';
 import { CareerGoal } from './CareerGoal';
 
 const server = mockServer();
 
 describe('CareerGoal', () => {
-  it('should return mock careerGoal data', async () => {
-    const response = await fetch(
-      'https://raw.githubusercontent.com/Phangster/Accredify-FE-Interview-Endpoint/main/goals.json',
-    );
-    const data = await response.json();
-    expect(data).toEqual(careerGoal); // Проверка, что данные корректно возвращаются
-  });
-
   it('Rendered career goal', async () => {
     renderWithProvider(<CareerGoal />);
 
